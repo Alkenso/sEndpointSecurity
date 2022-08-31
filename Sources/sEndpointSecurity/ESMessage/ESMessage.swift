9dfd8d138f2e1020994e9b8414b77579a40fd97a
Alkenso (Vladimir Vashurkin) <alkensox@gmail.com>
2022-08-31 12:28:31 +0300

    Add public initializers for all Swift ES types

--- Sources/sEndpointSecurity/ESMessage/ESMessage.swift
+++ Sources/sEndpointSecurity/ESMessage/ESMessage.swift
@@ -42,12 +42,20 @@ public struct ESMessage: Equatable {
         case auth
         case notify(ESAuthResult)
     }
-}
-
-public struct ESFile: Equatable {
-    public var path: String
-    public var truncated: Bool
-    public var stat: stat
+    
+    public init(version: UInt32, time: timespec, machTime: UInt64, deadline: UInt64, process: ESProcess, seqNum: UInt64? = nil, action: ESMessage.Action, event: ESEvent, eventType: es_event_type_t, thread: ESThread? = nil, globalSeqNum: UInt64? = nil) {
+        self.version = version
+        self.time = time
+        self.machTime = machTime
+        self.deadline = deadline
+        self.process = process
+        self.seqNum = seqNum
+        self.action = action
+        self.event = event
+        self.eventType = eventType
+        self.thread = thread
+        self.globalSeqNum = globalSeqNum
+    }
 }
 
 public struct ESProcess: Equatable {
@@ -67,6 +75,55 @@ public struct ESProcess: Equatable {
     public var startTime: timeval? /* field available only if message version >= 3 */
     public var responsibleAuditToken: audit_token_t? /* field available only if message version >= 4 */
     public var parentAuditToken: audit_token_t? /* field available only if message version >= 4 */
+    
+    public init(auditToken: audit_token_t, ppid: pid_t, originalPpid: pid_t, groupID: pid_t, sessionID: pid_t, codesigningFlags: UInt32, isPlatformBinary: Bool, isESClient: Bool, cdHash: Data, signingID: String, teamID: String, executable: ESFile, tty: ESFile? = nil, startTime: timeval? = nil, responsibleAuditToken: audit_token_t? = nil, parentAuditToken: audit_token_t? = nil) {
+        self.auditToken = auditToken
+        self.ppid = ppid
+        self.originalPpid = originalPpid
+        self.groupID = groupID
+        self.sessionID = sessionID
+        self.codesigningFlags = codesigningFlags
+        self.isPlatformBinary = isPlatformBinary
+        self.isESClient = isESClient
+        self.cdHash = cdHash
+        self.signingID = signingID
+        self.teamID = teamID
+        self.executable = executable
+        self.tty = tty
+        self.startTime = startTime
+        self.responsibleAuditToken = responsibleAuditToken
+        self.parentAuditToken = parentAuditToken
+    }
+}
+
+public struct ESFile: Equatable {
+    public var path: String
+    public var truncated: Bool
+    public var stat: stat
+    
+    public init(path: String, truncated: Bool, stat: stat) {
+        self.path = path
+        self.truncated = truncated
+        self.stat = stat
+    }
+}
+
+public struct ESThread: Equatable {
+    public var threadID: UInt64
+    
+    public init(threadID: UInt64) {
+        self.threadID = threadID
+    }
+}
+
+public struct ESThreadState: Equatable {
+    public var flavor: Int32
+    public var state: Data
+    
+    public init(flavor: Int32, state: Data) {
+        self.flavor = flavor
+        self.state = state
+    }
 }
 
 public struct ESAuthResult: Equatable, Codable, RawRepresentable {
@@ -78,15 +135,6 @@ public struct ESAuthResult: Equatable, Codable, RawRepresentable {
     public init(rawValue: UInt32) {
         self.rawValue = rawValue
     }
-}
-
-public struct ESThread: Equatable {
-    public var threadID: UInt64
-}
-
-public struct ESThreadState: Equatable {
-    public var flavor: Int32
-    public var state: Data
 }
 
 public enum ESEvent: Equatable {
@@ -159,20 +207,39 @@ public extension ESEvent {
     struct Access: Equatable {
         public var mode: Int32
         public var target: ESFile
+        
+        public init(mode: Int32, target: ESFile) {
+            self.mode = mode
+            self.target = target
+        }
     }
     
     struct Chdir: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
     
     struct Chroot: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
     
     struct Clone: Equatable {
         public var source: ESFile
         public var targetDir: ESFile
         public var targetName: String
+        
+        public init(source: ESFile, targetDir: ESFile, targetName: String) {
+            self.source = source
+            self.targetDir = targetDir
+            self.targetName = targetName
+        }
     }
     
     struct CopyFile: Equatable {
@@ -182,11 +249,25 @@ public extension ESEvent {
         public var targetName: String
         public var mode: mode_t
         public var flags: Int32
+        
+        public init(source: ESFile, targetFile: ESFile? = nil, targetDir: ESFile, targetName: String, mode: mode_t, flags: Int32) {
+            self.source = source
+            self.targetFile = targetFile
+            self.targetDir = targetDir
+            self.targetName = targetName
+            self.mode = mode
+            self.flags = flags
+        }
     }
     
     struct Close: Equatable {
         public var modified: Bool
         public var target: ESFile
+        
+        public init(modified: Bool, target: ESFile) {
+            self.modified = modified
+            self.target = target
+        }
     }
     
     struct Create: Equatable {
@@ -199,20 +280,38 @@ public extension ESEvent {
             case existingFile(ESFile)
             case newPath(dir: ESFile, filename: String, mode: mode_t)
         }
+        
+        public init(destination: ESEvent.Create.Destination) {
+            self.destination = destination
+        }
     }
     
     struct DeleteExtAttr: Equatable {
         public var target: ESFile
         public var extattr: String
+        
+        public init(target: ESFile, extattr: String) {
+            self.target = target
+            self.extattr = extattr
+        }
     }
     
     struct Dup: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
     
     struct ExchangeData: Equatable {
         public var file1: ESFile
         public var file2: ESFile
+        
+        public init(file1: ESFile, file2: ESFile) {
+            self.file1 = file1
+            self.file2 = file2
+        }
     }
     
     struct Exec: Equatable {
@@ -220,88 +319,177 @@ public extension ESEvent {
         public var script: ESFile? /* field available only if message version >= 2 */
         public var cwd: ESFile? /* field available only if message version >= 3 */
         public var lastFD: Int32? /* field available only if message version >= 4 */
+        
+        public init(target: ESProcess, script: ESFile? = nil, cwd: ESFile? = nil, lastFD: Int32? = nil) {
+            self.target = target
+            self.script = script
+            self.cwd = cwd
+            self.lastFD = lastFD
+        }
     }
     
     struct Exit: Equatable {
         public var status: Int32
+        
+        public init(status: Int32) {
+            self.status = status
+        }
     }
     
     struct FileProviderMaterialize: Equatable {
         public var instigator: ESProcess
         public var source: ESFile
         public var target: ESFile
+        
+        public init(instigator: ESProcess, source: ESFile, target: ESFile) {
+            self.instigator = instigator
+            self.source = source
+            self.target = target
+        }
     }
     
     struct FileProviderUpdate: Equatable {
         public var source: ESFile
         public var targetPath: String
+        
+        public init(source: ESFile, targetPath: String) {
+            self.source = source
+            self.targetPath = targetPath
+        }
     }
     
     struct Fcntl: Equatable {
         public var target: ESFile
         public var cmd: Int32
+        
+        public init(target: ESFile, cmd: Int32) {
+            self.target = target
+            self.cmd = cmd
+        }
     }
     
     struct Fork: Equatable {
         public var child: ESProcess
+        
+        public init(child: ESProcess) {
+            self.child = child
+        }
     }
     
     struct FsGetPath: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
     
     struct GetTask: Equatable {
         public var target: ESProcess
+        
+        public init(target: ESProcess) {
+            self.target = target
+        }
     }
     
     struct GetTaskRead: Equatable {
         public var target: ESProcess
+        
+        public init(target: ESProcess) {
+            self.target = target
+        }
     }
     
     struct GetTaskInspect: Equatable {
         public var target: ESProcess
+        
+        public init(target: ESProcess) {
+            self.target = target
+        }
     }
     
     struct GetTaskName: Equatable {
         public var target: ESProcess
+        
+        public init(target: ESProcess) {
+            self.target = target
+        }
     }
     
     struct GetAttrList: Equatable {
         public var attrlist: attrlist
         public var target: ESFile
+        
+        public init(attrlist: attrlist, target: ESFile) {
+            self.attrlist = attrlist
+            self.target = target
+        }
     }
     
     struct GetExtAttr: Equatable {
         public var target: ESFile
         public var extattr: String
+        
+        public init(target: ESFile, extattr: String) {
+            self.target = target
+            self.extattr = extattr
+        }
     }
     
     struct IOKitOpen: Equatable {
         public var userClientType: UInt32
         public var userClientClass: String
+        
+        public init(userClientType: UInt32, userClientClass: String) {
+            self.userClientType = userClientType
+            self.userClientClass = userClientClass
+        }
     }
     
     struct KextLoad: Equatable {
         public var identifier: String
+        
+        public init(identifier: String) {
+            self.identifier = identifier
+        }
     }
     
     struct KextUnload: Equatable {
         public var identifier: String
+        
+        public init(identifier: String) {
+            self.identifier = identifier
+        }
     }
     
     struct Link: Equatable {
         public var source: ESFile
         public var targetDir: ESFile
         public var targetFilename: String
+        
+        public init(source: ESFile, targetDir: ESFile, targetFilename: String) {
+            self.source = source
+            self.targetDir = targetDir
+            self.targetFilename = targetFilename
+        }
     }
     
     struct ListExtAttr: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
     
     struct Lookup: Equatable {
         public var sourceDir: ESFile
         public var relativeTarget: String
+        
+        public init(sourceDir: ESFile, relativeTarget: String) {
+            self.sourceDir = sourceDir
+            self.relativeTarget = relativeTarget
+        }
     }
     
     struct MMap: Equatable {
@@ -310,57 +498,116 @@ public extension ESEvent {
         public var flags: Int32
         public var filePos: UInt64
         public var source: ESFile
+        
+        public init(protection: Int32, maxProtection: Int32, flags: Int32, filePos: UInt64, source: ESFile) {
+            self.protection = protection
+            self.maxProtection = maxProtection
+            self.flags = flags
+            self.filePos = filePos
+            self.source = source
+        }
     }
     
     struct Mount: Equatable {
         public var statfs: statfs
+        
+        public init(statfs: statfs) {
+            self.statfs = statfs
+        }
     }
     
     struct MProtect: Equatable {
         public var protection: Int32
         public var address: user_addr_t
         public var size: user_size_t
+        
+        public init(protection: Int32, address: user_addr_t, size: user_size_t) {
+            self.protection = protection
+            self.address = address
+            self.size = size
+        }
     }
     
     struct Open: Equatable {
         public var fflag: Int32
         public var file: ESFile
+        
+        public init(fflag: Int32, file: ESFile) {
+            self.fflag = fflag
+            self.file = file
+        }
     }
     
     struct ProcCheck: Equatable {
         public var target: ESProcess?
         public var type: es_proc_check_type_t
         public var flavor: Int32
+        
+        public init(target: ESProcess? = nil, type: es_proc_check_type_t, flavor: Int32) {
+            self.target = target
+            self.type = type
+            self.flavor = flavor
+        }
     }
     
     struct ProcSuspendResume: Equatable {
         public var target: ESProcess?
         public var type: es_proc_suspend_resume_type_t
+        
+        public init(target: ESProcess? = nil, type: es_proc_suspend_resume_type_t) {
+            self.target = target
+            self.type = type
+        }
     }
     
     struct PtyClose: Equatable {
         public var dev: dev_t
+        
+        public init(dev: dev_t) {
+            self.dev = dev
+        }
     }
     
     struct PtyGrant: Equatable {
         public var dev: dev_t
+        
+        public init(dev: dev_t) {
+            self.dev = dev
+        }
     }
     
     struct Readdir: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
     
     struct Readlink: Equatable {
         public var source: ESFile
+        
+        public init(source: ESFile) {
+            self.source = source
+        }
     }
     
     struct RemoteThreadCreate: Equatable {
         public var target: ESProcess
         public var threadState: ESThreadState?
+        
+        public init(target: ESProcess, threadState: ESThreadState? = nil) {
+            self.target = target
+            self.threadState = threadState
+        }
     }
     
     struct Remount: Equatable {
         public var statfs: statfs
+        
+        public init(statfs: statfs) {
+            self.statfs = statfs
+        }
     }
     
     struct Rename: Equatable {
@@ -371,11 +618,22 @@ public extension ESEvent {
             case existingFile(ESFile)
             case newPath(dir: ESFile, filename: String)
         }
+        
+        
+        public init(source: ESFile, destination: ESEvent.Rename.Destination) {
+            self.source = source
+            self.destination = destination
+        }
     }
     
     struct SearchFS: Equatable {
         public var attrlist: attrlist
         public var target: ESFile
+        
+        public init(attrlist: attrlist, target: ESFile) {
+            self.attrlist = attrlist
+            self.target = target
+        }
     }
     
     struct SetACL: Equatable {
@@ -384,64 +642,127 @@ public extension ESEvent {
         
         //  currently not supported
         //  public var acl: acl_t
+        
+        public init(target: ESFile, setOrClear: es_set_or_clear_t) {
+            self.target = target
+            self.setOrClear = setOrClear
+        }
     }
     
     struct SetAttrList: Equatable {
         public var attrlist: attrlist
         public var target: ESFile
+        
+        public init(attrlist: attrlist, target: ESFile) {
+            self.attrlist = attrlist
+            self.target = target
+        }
     }
     
     struct SetExtAttr: Equatable {
         public var target: ESFile
         public var extattr: String
+        
+        public init(target: ESFile, extattr: String) {
+            self.target = target
+            self.extattr = extattr
+        }
     }
     
     struct SetFlags: Equatable {
         public var flags: UInt32
         public var target: ESFile
+        
+        public init(flags: UInt32, target: ESFile) {
+            self.flags = flags
+            self.target = target
+        }
     }
     
     struct SetMode: Equatable {
         public var mode: mode_t
         public var target: ESFile
+        
+        public init(mode: mode_t, target: ESFile) {
+            self.mode = mode
+            self.target = target
+        }
     }
     
     struct SetOwner: Equatable {
         public var uid: uid_t
         public var gid: gid_t
         public var target: ESFile
+        
+        public init(uid: uid_t, gid: gid_t, target: ESFile) {
+            self.uid = uid
+            self.gid = gid
+            self.target = target
+        }
     }
     
     struct SetUID: Equatable {
         public var uid: uid_t
+        
+        public init(uid: uid_t) {
+            self.uid = uid
+        }
     }
     
     struct SetREUID: Equatable {
         public var ruid: uid_t
         public var euid: uid_t
+        
+        public init(ruid: uid_t, euid: uid_t) {
+            self.ruid = ruid
+            self.euid = euid
+        }
     }
     
     struct Signal: Equatable {
         public var sig: Int32
         public var target: ESProcess
+        
+        public init(sig: Int32, target: ESProcess) {
+            self.sig = sig
+            self.target = target
+        }
     }
     
     struct Stat: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
     
     struct Trace: Equatable {
         public var target: ESProcess
+        
+        public init(target: ESProcess) {
+            self.target = target
+        }
     }
     
     struct Truncate: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
     
     struct UipcBind: Equatable {
         public var dir: ESFile
         public var filename: String
         public var mode: mode_t
+        
+        public init(dir: ESFile, filename: String, mode: mode_t) {
+            self.dir = dir
+            self.filename = filename
+            self.mode = mode
+        }
     }
     
     struct UipcConnect: Equatable {
@@ -449,24 +770,51 @@ public extension ESEvent {
         public var domain: Int32
         public var type: Int32
         public var `protocol`: Int32
+        
+        public init(file: ESFile, domain: Int32, type: Int32, protocol: Int32) {
+            self.file = file
+            self.domain = domain
+            self.type = type
+            self.protocol = `protocol`
+        }
     }
     
     struct Unlink: Equatable {
         public var target: ESFile
         public var parentDir: ESFile
+        
+        public init(target: ESFile, parentDir: ESFile) {
+            self.target = target
+            self.parentDir = parentDir
+        }
     }
     
     struct Unmount: Equatable {
-        public var statfs: statfs;
+        public var statfs: statfs
+        
+        
+        public init(statfs: statfs) {
+            self.statfs = statfs
+        }
     }
     
     struct Utimes: Equatable {
         public var target: ESFile
-        public var aTime: timespec;
-        public var mTime: timespec;
+        public var aTime: timespec
+        public var mTime: timespec
+        
+        public init(target: ESFile, aTime: timespec, mTime: timespec) {
+            self.target = target
+            self.aTime = aTime
+            self.mTime = mTime
+        }
     }
     
     struct Write: Equatable {
         public var target: ESFile
+        
+        public init(target: ESFile) {
+            self.target = target
+        }
     }
 }
